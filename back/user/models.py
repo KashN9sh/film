389233f85aa892ed2ebb4fe310a137156b802f4b
KorolevRoman228 <kash244@yandex.ru>
2022-12-06 19:27:03 +0300

    что то делал, устал и забыл что делал, вот

---
+++ back/user/models.py
@@ -0,0 +1,11 @@
+from back.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    nickname = Column(String)
+    is_active = Column(Boolean, default=True)
