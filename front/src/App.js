bef06f85b51c028427d7bb7f0edee445ff50328e
KorolevRoman228 <kash244@yandex.ru>
2023-01-06 17:22:54 +0300

    Начинаем фронт

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import bootstrap from 'bootstrap'
+import { Component } from 'react'
+
+class Films extends Component{
+
+render() {
+    return(
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
 
 function App() {
+  axios({
+        method: 'get',
+        url: '/film/',
+    })
+        .then(function (response) {
+          console.log(response.data)
+        })
+        .catch(function (error) {
+          console.log(error)
+
+        })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="container text-center ">
+      <div class="row align-items-start align-middle">
+        <div class="col border p-3 m-3 rounded">
+
+        </div>
+
+        <div class="col border p-3 m-3 rounded" align="center">
+          <Films/>
+        </div>
+
+        <div class="col border p-3 m-3 rounded">
+          One of three columns
+        </div>
+      </div>
+  </div>
   );
 }
 
